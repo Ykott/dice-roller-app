f2cae9a1213930780b5e2e136a9d6af328d6ed67
Scott Klosen <scott.klosen@gmail.com>
2026-04-06 14:29:14 -0400

    dice-03: Build the Die component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Die from "./components/Die";
+
 function App() {
+  let val = ["A","E","i"];
   return (
-    <div className="flex min-h-screen items-center justify-center text-3xl">
-      Dice Roller
+    <div className="flex min-h-screen flex-col items-center justify-center gap-8 bg-green-700">
+      <h1 className="text-4xl font-bold text-white">Dice Roller</h1>
+      <Die></Die>
+      {val.map((item)=> <span key={item}>{item}</span>)}
     </div>
   );
 }
